dd83091849b6baca6ac98c73f77344622d156c43
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-04 19:28:09 +0400

    filter query added

--- express/index.js
+++ express/index.js
@@ -25,13 +25,38 @@ app.get('/', (req, res) => {
     res.send('Server is true !!!')
 })
 
-app.get('/publications', (req,res) => {
-    connection.query('SELECT * FROM publication', (err,data) => {
-        if(!data){
+// app.get('/publications', (req, res) => {
+//     connection.query('SELECT * FROM publication', (err, data) => {
+//         if (!data) {
+//             return res.status(400).send()
+//         }
+//         res.status(200).send(data)
+//     })
+// })
+
+app.get('/aze', (req, res) => {
+    connection.query("SELECT * FROM publication"), (err, data) => {
+        if (!data) {
             return res.status(400).send()
         }
         res.status(200).send(data)
-    })
+    }
+})
+app.get('/tr', (req, res) => {
+    connection.query('SELECT * FROM publication WHERE publication_name=' + "'tr'"), (err, data) => {
+        if (!data) {
+            return res.status(400).send()
+        }
+        res.status(200).send(data)
+    }
+})
+app.get('/eng', (req, res) => {
+    connection.query('SELECT * FROM publication WHERE publication_name=' + "'eng'"), (err, data) => {
+        if (!data) {
+            return res.status(400).send()
+        }
+        res.status(200).send(data)
+    }
 })
 
 app.listen(3000, function () {
