b75a25112ec69ead8149862205c61b332fcf4a76
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-04 18:09:01 +0400

    publications table added to express

--- express/index.js
+++ express/index.js
@@ -7,6 +7,31 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: 'admin',
-    database: 'Aplollo'
+    database: 'Apollo'
 })
 
+connection.connect((err) => {
+    if (!err) {
+        console.log('Connected to MYSQL')
+    }
+})
+
+const app = express();
+app.use((express.json()));
+
+app.get('/', (req, res) => {
+    res.send('Server is true !!!')
+})
+
+app.get('/publications', (req,res) => {
+    connection.query('SELECT * FROM publication', (err,data) => {
+        if(!data){
+            return res.status(400).send()
+        }
+        res.status(200).send(data)
+    })
+})
+
+app.listen(3000, function () {
+    console.log('Server started add 3000 port')
+})
